af828874fd6c6084d186a51aad71bf5462b1c95e
Mariano Hurtado de Mendoza <theunfactoringguru@gmail.com>
2023-05-29 09:12:32 -0600

    v1.0.0 done

--- js/index.js
+++ js/index.js
@@ -12,9 +12,19 @@ const contactInfoButton = document.getElementById('contact-button');
 const contactInfo = document.getElementById('contact');
 const professionalInfoButton = document.getElementById('professional-info-button');
 const professionalInfo = document.getElementById('professional-info');
+const studiesInfoButton = document.getElementById('studies-button');
+const studiesInfo = document.getElementById('studies');
+const experienceInfoButton = document.getElementById('experience-button');
+const experienceInfo = document.getElementById('experience');
 contactInfoButton.addEventListener('click', function () {
     turnModal(contactInfo);
 });
 professionalInfoButton.addEventListener('click', function () {
     turnModal(professionalInfo);
+});
+studiesInfoButton.addEventListener('click', function () {
+    turnModal(studiesInfo);
+});
+experienceInfoButton.addEventListener('click', function () {
+    turnModal(experienceInfo);
 });
